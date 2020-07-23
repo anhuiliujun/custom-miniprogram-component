75208ad88011c947fd341ea553411c58140798cb
issac lau <issaclau@issacdeMacBook-Pro.local>
2020-07-23 16:11:11 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  globals: {
+    wx: true,
+    my: true,
+    getApp: true,
+    App: true,
+    Component: true,
+    Page: true,
+  },
+};
---
+++ src/button/index.ts
@@ -0,0 +1,4 @@
+const a = 'xxx';
+Component({});
+
+console.log(a);
